44a6ecb325ea786f5da692eeec64773e77342d6c
Nayeli Gutierrez <yuridia.gutierrez16@tectijuana.edu.mx>
2020-10-14 18:02:30 -0700

    Practice 2 created and moditified

---
+++ Practice/Practice_2.r
@@ -0,0 +1,35 @@
+#Functions
+
+#Practice find 20 more functions in R and make an example of it.
+#one
+na.fail () #Show an error if there are NA swims
+#two
+match (,) # Returns a vector of the size of the first variable with the elements that are in the second
+#3
+choose (n, k) #Calculation of combinations in k with n repetitions formula n! / [(n-k)! k!]
+#4
+mean () #Returns the average of the elements
+#5
+median () #Returns the median of the elements
+# 6
+rev () #Reverse the order of elements
+# 7
+sort () # Sort the elements of x in ascending order
+# 8
+rank () # Align the elements of x
+# 9
+unique () # Returns a similar object but suppresses duplicate elements
+# 10
+plot () #Organize data on y-axis ordered by x
+#11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# 19
+# 20
+
+
