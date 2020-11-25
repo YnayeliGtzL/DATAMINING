149c4753223533b220530db6f3d1ad6ea142e45a
Nayeli Gutierrez <yuridia.gutierrez16@tectijuana.edu.mx>
2020-11-24 23:25:10 -0800

    Traslate of commits on evaluative practice

--- Unit_2/Evaluative_Practice/Evaluative_Practice_U2.r
+++ Unit_2/Evaluative_Practice/Evaluative_Practice_U2.r
@@ -1,70 +1,69 @@
-#Abrir la ruta correspondiente en donde se guarda el archivo para importar los datos
+#Open the corresponding path where the file is saved to import the data
 getwd()
 setwd("C:/Users/yurid/OneDrive/Documentos/Escuela/2_Mineria de datos/Unit_2")
 getwd()
-# Se cargan los datos a un dataframe para su manipulacion
+# Data is loaded into a dataframe for manipulation
 
 dataset <- read.csv('Project-Data.csv')
 
-#Segunda opcion para cargar los datos y no tener que modificar el codigo
+#Second option to load the data and not have to modify the code
 dataset <- read.csv(file.choose())
 
-# Se observa el dataframe para observacion y analisis
+# The dataframe is observed for observation and analysis
 summary(dataset)
 
-#Se cargan las librerias necesarias para la manipulacion de los datos
+#The necessary libraries for data manipulation are loaded
 library(ggplot2)
 
-#Para la realizacion de filtros se utilizo la libreria dply
+#For the realization of filters the dply library was used
 #install.packages("dplyr")
 library(dplyr)
 
-#Para cambiar al tipo de letra que necesitamos, instalamos la libreria 
+#To change the font we need, we install the library
 #install.packages("extrafont")
 
-#se carga la libreria de las fuentes
+#the font library is loaded
 library(extrafont)
 
-#Despues importamos todas las fuentes dentro de nuestro sistema, esto puede cambiar dependiendo tu SO
+#After we import all the fonts into our system, this may change depending on your OS
 font_import()
 
-#ejecutamos el comando para visualizar las fuentes y que r las pueda reconocer 
+#we execute the command to visualize the sources and that r can recognize them
 fonts()
 
-# Se crea un nuevo dataframe filtrando los datos para solo agregar los generos solicitados
+# A new dataframe is created filtering the data to only add the requested genres
 GenreF<-filter(dataset, Genre %in% c("action","adventure", 
                                      "animation","comedy", "drama"))
-# A partir del dataframe anterior se vulve a filtrar la informacion para solo tomar los estudios solicitados
+#From the previous data frame, the information is filtered again to only take the requested studies
 StudioF<- filter (GenreF, Studio %in% c ("Buena Vista Studios",
                                           "Fox", "Paramount Pictures",
                                           "Sony", "Universal","WB"))
-# Se crea una variable en la cual vamos a cargar el ploteo de los datos que contendra para los ejex X y Y
-#que seran genericos para el plot
+# A variable is created in which we will load the plot of the data that it will contain for the X and Y axes that will be generic for the plot.
 u <- ggplot(StudioF, aes(x=Genre, y=Gross...US))
-#Agregamos la geometria de Jitter para los estudios  
+#We add the Jitter geometry for the studies
 j <- u + geom_jitter(aes(color=Studio, size= Budget...mill.)) +
   scale_size_continuous(range = c(2, 5),                         
                         trans = scales::exp_trans(base = 1.2))
-#Visualizacion de nuestro plot
+#Visualization of our plot
 j
-#Agregamos boxplot para agrupar por genero  y Gross colocando una transparencia media
+#We add boxplot to group by gender and Gross placing a medium transparency
 g <-j + geom_boxplot(alpha=0.2,  outlier.colour = NA)
-#Visualizacion de nuestro plot
+#Visualization of our plot
 g
-#Colocamos el titulo de nuestro plot
+#We place the title of our plot
 t <-g + ggtitle("Domestic Gross % by Genre")
-#Visualizacion de nuestro plot
+#Visualization of our plot
 t
-#Colocamos el nombre de los ejes X y Y
+#We put the name of the X and Y axes
 e <- t + xlab("Genre") + ylab("Gross % US") 
-#Visualizacion de nuestro plot
+#Visualization of our plot
 e
-#Añadimos el tema para los labels
+#We add the theme for the labels
 th<-e + theme(axis.title.x = element_text(color = "Purple", size=15),
               axis.title.y = element_text(color = "Purple", size=15),
               plot.title= element_text(size = 25,hjust=0.5)
               ,text = element_text(family ="Comic Sans MS")
 ) + labs(size="Budget $ M")
-#Visualizacion de nuestro plot
+#Visualization of our plot
 th
 
