713dacbb4d2f0c76c67bdac719d71726927c2f74
Nayeli Gutierrez <yuridia.gutierrez16@tectijuana.edu.mx>
2020-12-11 20:54:05 -0800

    Add confucion matrix impor library add visualising training practice unit 3

--- Unit_3/Evaluative_PracticeU3/Evaluative_PracticeU3.r
+++ Unit_3/Evaluative_PracticeU3/Evaluative_PracticeU3.r
@@ -29,3 +29,18 @@ classifier = naiveBayes(formula = Purchased ~ .,
 
 y_pred = predict(classifier, newdata = test_set[-3])
 y_pred
+
+cm = table(test_set[, 3], y_pred)
+cm
+# Visualising training
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3],
+     main = 'Naive Bayes (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
