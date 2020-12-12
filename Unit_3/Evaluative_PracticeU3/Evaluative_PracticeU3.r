2bd8b2f96d6fd92948331c7f24cda1ef7f8f5723
Tu nombre <the_gamer_of_halo@hotmail.com>
2020-12-11 20:50:33 -0800

    update in evaluative practice

--- Unit_3/Evaluative_PracticeU3/Evaluative_PracticeU3.r
+++ Unit_3/Evaluative_PracticeU3/Evaluative_PracticeU3.r
@@ -8,3 +8,24 @@ dataset = read.csv('Social_Network_Ads.csv')
 dataset = dataset[3:5]
 
 dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+
+#install.packages('e1071')
+library(e1071)
+classifier = naiveBayes(formula = Purchased ~ .,
+                        data = training_set,
+                        type = 'C-classification',
+                        kernel = 'linear')
+
+y_pred = predict(classifier, newdata = test_set[-3])
+y_pred
